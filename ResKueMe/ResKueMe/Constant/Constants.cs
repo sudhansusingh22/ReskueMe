e93f5958ccc67bcc613d0be8065e39bfb8ebb796
Sudhansu Singh <sudhansusingh22@gmail.com>
2016-02-22 12:40:16 -0500

    removed app id

--- ResKueMe/ResKueMe/Constant/Constants.cs
+++ ResKueMe/ResKueMe/Constant/Constants.cs
@@ -8,9 +8,9 @@ namespace ResKueMe.Constant
 {
     public static class Constants
     {
-        public static readonly string FacebookAppId = "218520768272008";
+        public static readonly string FacebookAppId = "************";
         public static readonly string FBActionBaseUri = "Base URL for the website where your meal pages exist";
         public static readonly string FacebookAppGraphAction = "<Your namespace>";
-        public static readonly string FacebookClientSecret = "22f2d2ab2ead1d50307a9f6cb3d8b2f7";
+        public static readonly string FacebookClientSecret = "*****************";
     }
 }
